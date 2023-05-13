9a6113c034dd377c892f7daa7dae6cf2e5c0db7b
Konstantin Zaidelman <k.zaydelman.callrealty@gmail.com>
2023-05-13 19:42:48 +0300

    Create Blog resource

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Blog;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class BlogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View
+    {
+        return view('blog.index', [
+            'records' => Blog::with('user')->latest()->get()
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $attributes = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        $request->user()->blogs()->create($attributes);
+
+        return redirect(route('blog.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Blog $blog)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Blog $blog): View
+    {
+        $this->authorize('update', $blog);
+
+        return view('blog.edit', [
+            'blog' => $blog,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Blog $blog)
+    {
+        $this->authorize('update', $blog);
+
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        $blog->update($validated);
+
+        return redirect(route('blog.index'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Blog $blog)
+    {
+        //
+    }
+}
